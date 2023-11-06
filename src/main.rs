a77c30788ff6c68a8f621737222d3d9454351b7a
Zi Nean Teoh <zinean00@gmail.com>
2023-11-06 14:06:09 -0600

    add a dynamic route /{name} greeting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use tokio;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -11,17 +12,25 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    // format!("Hello {}!", &name)
+    HttpResponse::Ok().body(format!("Hello {}!", &name))
+}
+
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-#[actix_web::main]
+// this macro enables async/await syntax in the main function
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .route("/{name}", web::get().to(greet))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
